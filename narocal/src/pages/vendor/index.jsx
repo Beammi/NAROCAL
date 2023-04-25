89d6480423881d652d823788aa4ec5c2ce70c68d
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-04-26 00:28:46 +0700

    Add vendor profile

--- narocal/src/pages/vendor/index.jsx
+++ narocal/src/pages/vendor/index.jsx
@@ -1,14 +1,25 @@
 import VendorNavBar from '@/components/VendorNavBar'
 import Image from "next/image"
-import ProfileVendorMock from "../pages/assets/vendor_profile_mock.png"
+import P from "@/components/text/P"
+import ProfileVendorMock from "../../pages/assets/vendor_profile_mock.png"
 export default function VendorMain(){
-    const router = useRouter();
+    // const router = useRouter();
     return(
         <div>
             <VendorNavBar></VendorNavBar>
-            <div>
-                <figure><Image src={ProfileVendorMock} alt="Vendor's Profile"/></figure>
-
+            <div className='flex justify-center p-4'>
+                <div className='felx flex-col items-center'>
+                    <figure><Image src={ProfileVendorMock} alt="Vendor's Profile" width={200}/></figure>
+                    <P text="Marie Shop" style='text-base xs:text-xs'></P>
+                    <div className="rating rating-md p-2">
+                        <input type="radio" name="rating" className="mask mask-star-2 bg-secondary" />
+                        <input type="radio" name="rating" className="mask mask-star-2 bg-secondary" />
+                        <input type="radio" name="rating" className="mask mask-star-2 bg-secondary" />
+                        <input type="radio" name="rating" className="mask mask-star-2 bg-secondary" />
+                        <input type="radio" name="rating" className="mask mask-star-2 bg-secondary" />
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
