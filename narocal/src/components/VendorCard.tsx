3a924613e6fe53dfbc9099c4d23cc1793cedf821
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-04-28 00:01:36 +0700

    Change language to be list

--- narocal/src/components/VendorCard.tsx
+++ narocal/src/components/VendorCard.tsx
@@ -10,7 +10,7 @@ interface VendorCard {
   imageAlt?: string
   link?: string
   name: string
-  language: string
+  language: []
   exchange_rate: string
   shopping_rate: string
   image_star_rate?: string
@@ -28,11 +28,14 @@ const VendorCard: React.FunctionComponent<VendorCard> = ({
     return (
         <>
             <Link href={{pathname:'/vendor/[slug]', query: {slug: link}}}>
-                <div className="card lg:card-side sm:card-compact shadow-xl w-5/6 rounded-xl bg-background my-2 ml-16">
+                <div className="card lg:card-side sm:card-compact shadow-xl md:w5/6 rounded-xl bg-background p-4 m-4">
                     <figure><Image className="mx-8 sm:pt-2" src={ProfilePicTemp} alt="product pic temp" width={100}/></figure>
                     <div className="card-body bg-background rounded-xl">
                         <h2 className="card-title sm:text-ellipsis sm:text-xs md:text-xl">{name}</h2>
-                        <p className="sm:text-ellipsis sm:text-xs md:text-base">Languages: {language}</p>
+                        <p className="sm:text-ellipsis sm:text-xs md:text-base">Languages: {language}
+                        {/* .map(l => (
+                        <p className="font-light md:text-sm m:text-xs p-2">{l}</p>)) */}
+                        </p>
                         <p className="sm:text-ellipsis sm:text-xs md:text-base">Exchange Rate: {exchange_rate}</p>
                         <p className="sm:text-ellipsis sm:text-xs md:text-base">Shopping Rate: {shopping_rate}</p>
                         <div className="card-actions justify-start rating">
--- narocal/src/pages/vendor/index.jsx
+++ narocal/src/pages/vendor/index.jsx
@@ -10,20 +10,21 @@ import VendorCard from '@/components/VendorCard'
 export default function VendorsMain(){
     // const router = useRouter();
     const vendors = [
-        {id: "1", name: "Chris Evan", language: "English", exchange_rate: "1 pound = 40 Baht", shopping_rate: "300 Baht" },
-        {id: "2", name: "Yoko Ano", language: "English", exchange_rate: "1 pound = 40 Baht", shopping_rate: "300 Baht"},
-        {id: "3", name: "Yoshida", language: "English", exchange_rate: "1 pound = 40 Baht", shopping_rate: "300 Baht"},
-        {id: "4", name: "Miyawaki Sakura", language: "English", exchange_rate: "1 pound = 40 Baht", shopping_rate: "300 Baht"}
+        {id: "1", name: "Chris Evan", language: ["English ","Spanish "], exchange_rate: "1 pound = 40 Baht", shopping_rate: "300 Baht" },
+        {id: "2", name: "Yoko Ano", language: ["English ","Japanese "], exchange_rate: "1 pound = 40 Baht", shopping_rate: "350 Baht"},
+        {id: "3", name: "Yoshida", language: ["English ","Japanese "], exchange_rate: "1 pound = 40 Baht", shopping_rate: "200 Baht"},
+        {id: "4", name: "Miyawaki Sakura", language: ["English ","Japanese "], exchange_rate: "1 pound = 40 Baht", shopping_rate: "250 Baht"},
+        {id: "5", name: "John Doe", language: ["English ","Chinese "], exchange_rate: "1 Yuan = 0.55 Baht", shopping_rate: "250 Baht"}
     ]
     return(
         <div>
             <VendorNavBar></VendorNavBar>
             
             <div className='flex justify-center bg-test'>
-                <div className='flex flex-col m-6'>
-                    <div className='md:w-5/6'>
+                <div className='flex flex-col m-6 items-center'>
+                    <div className='md:w-5/6 justify-items-center'>
                         <h3 className='md:text-xl xs:text-xs bg-background p-2 rounded-t-lg'>Vendor</h3>
-                        <ul className='grid justify-items-center md:grid-cols-2 sm:grid-flow-row sm:auto-rows-auto gap-4 overflow-x-auto overflow-contain p-10 bg-background rounded-b-lg'>
+                        <ul className='grid justify-items-center sm:justify-items-center md:grid-cols-2 sm:grid-flow-row sm:auto-rows-auto gap-4 overflow-x-auto overflow-contain bg-background rounded-b-lg'>
                                 {vendors.map((vendor) =>(
                                     <VendorCard name={vendor.name} language={vendor.language} exchange_rate={vendor.exchange_rate} shopping_rate={vendor.shopping_rate} link={vendor.id}></VendorCard>
                                 ))}
