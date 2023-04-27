f47a23d0fa6bead83c84693134f1fd1cc59d5244
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-04-27 20:53:59 +0700

    Add Link to card

--- narocal/src/components/VendorCard.tsx
+++ narocal/src/components/VendorCard.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image"
 import P from "../components/text/P"
 import ProfilePicTemp from "../pages/assets/profile_pic_temp.png"
+import Link from 'next/link'
+import { Url } from "url"
+
 
 interface VendorCard {
   image?: string | any
   imageAlt?: string
-
+  link?: string
   name: string
   language: string
   exchange_rate: string
@@ -16,6 +19,7 @@ interface VendorCard {
 const VendorCard: React.FunctionComponent<VendorCard> = ({
   image,
   imageAlt,
+  link,
   name,
   language,
   exchange_rate,
@@ -23,22 +27,25 @@ const VendorCard: React.FunctionComponent<VendorCard> = ({
 }) => {
     return (
         <>
-            <div className="card lg:card-side sm:card-compact shadow-xl w-5/6 rounded-xl bg-background my-2 ml-16">
-                <figure><Image className="mx-8 sm:pt-2" src={ProfilePicTemp} alt="product pic temp" width={100}/></figure>
-                <div className="card-body bg-background rounded-xl">
-                    <h2 className="card-title sm:text-ellipsis sm:text-xs md:text-xl">{name}</h2>
-                    <p className="sm:text-ellipsis sm:text-xs md:text-base">Languages: {language}</p>
-                    <p className="sm:text-ellipsis sm:text-xs md:text-base">Exchange Rate: {exchange_rate}</p>
-                    <p className="sm:text-ellipsis sm:text-xs md:text-base">Shopping Rate: {shopping_rate}</p>
-                    <div className="card-actions justify-start rating">
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"  />
-                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <Link href={{pathname:'/vendor/[slug]', query: {slug: link}}}>
+                <div className="card lg:card-side sm:card-compact shadow-xl w-5/6 rounded-xl bg-background my-2 ml-16">
+                    <figure><Image className="mx-8 sm:pt-2" src={ProfilePicTemp} alt="product pic temp" width={100}/></figure>
+                    <div className="card-body bg-background rounded-xl">
+                        <h2 className="card-title sm:text-ellipsis sm:text-xs md:text-xl">{name}</h2>
+                        <p className="sm:text-ellipsis sm:text-xs md:text-base">Languages: {language}</p>
+                        <p className="sm:text-ellipsis sm:text-xs md:text-base">Exchange Rate: {exchange_rate}</p>
+                        <p className="sm:text-ellipsis sm:text-xs md:text-base">Shopping Rate: {shopping_rate}</p>
+                        <div className="card-actions justify-start rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"  />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
+            
             
         </>
     )
