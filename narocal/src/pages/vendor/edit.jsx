493cef2600b2aaca7e83f8a4319f61c3c896451c
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-05-01 15:45:12 +0700

    Add vendor profile edit page

---
+++ narocal/src/pages/vendor/edit.jsx
@@ -0,0 +1,58 @@
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
+export default function VendorEditProfile() {
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState(null)
+  const [website, setWebsite] = useState(null)
+  const [avatar_url, setAvatarUrl] = useState(null)
+  return (
+    <div>
+      <form className="form-widget">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" disabled />
+        </div>
+        <div>
+          <label htmlFor="username">Name</label>
+          <input
+            id="username"
+            type="text"
+            required
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="website">Website</label>
+          <input
+            id="website"
+            type="url"
+            value={website || ""}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button
+            className="button block primary"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="button block"
+            type="button"
+          >
+            Sign Out
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
