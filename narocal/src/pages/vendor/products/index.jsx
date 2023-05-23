9f854fff71c16fc14d298831e6486b7d9238f088
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-05-23 09:19:08 +0700

    Add Design to Product Page

--- narocal/src/pages/vendor/products/index.jsx
+++ narocal/src/pages/vendor/products/index.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link"
-
+import VendorNavBar from "@/components/vendors/VendorNavBar"
 
 export default function VendorProduct() {
     return(
+        
         <div>
-            <Link href="vendor/products/create">Create Product </Link>
-            <p>Vendor Product</p>
+
+            <VendorNavBar></VendorNavBar>
+            <div className="divide-y-2">
+                <div className="p-6">
+                    <Link href="products/create" className="hover:text-secondary md:text-lg sm:text-sm">Create Product </Link>
+                </div>
+
+            </div>
+            
+            <p className="p-6">Vendor Product</p>
         </div>
     )
 }
