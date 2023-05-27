7cdbb346a2d221dbb8375e21c2a296804d3e15df
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-05-28 01:12:17 +0700

    Add Qrcode

--- narocal/src/pages/payment/index.jsx
+++ narocal/src/pages/payment/index.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import ProductPicTemp from "../../pages/assets/bank-transfer.jpg"
+import Image from "next/image"
 
 export default function PaymentHome() {
   return (
     <div className="flex flex-col items-center h-screen gap-10 mt-10">
             <h2 className="text-center text-3xl font-bold">Pay Here</h2>
+            <figure><Image src={ProductPicTemp} alt="product pic temp" className="pt-2" width={280}/></figure>
             <Link className="btn btn-lg bg-neutral-400 text-black border-none hover:bg-secondary" href="/payment/successful">Confirm</Link>
         </div>
   )
