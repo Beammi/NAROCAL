44d3895eaa51b93a31a3fa0ce13ab843dcdaedc0
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-05-27 17:55:03 +0700

    Fix chat

--- narocal/src/pages/chat/[chatid]/index.jsx
+++ narocal/src/pages/chat/[chatid]/index.jsx
@@ -1,172 +1,85 @@
-import { useRouter } from "next/router"
-import { useState, useEffect } from "react"
-import { supabase } from "lib/supabaseClient"
-import VendorNavBar from "@/components/vendors/VendorNavBar"
-import ChatChannel from "@/components/chat/ChatChannel"
-import CustomerNavbar from "@/components/CustomerNavbar"
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { supabase } from "lib/supabaseClient";
+import VendorNavBar from "@/components/vendors/VendorNavBar";
+import ChatChannel from "@/components/chat/ChatChannel";
+import CustomerNavbar from "@/components/CustomerNavbar";
 
-export const revalidate = 60
-
-export default function Chat() {
-  const router = useRouter()
-  const [loading, setLoading] = useState(true)
-  const [email, setEmail] = useState("")
-  const [chats, setChats] = useState(null)
-  const [lenCus, setLenCus] = useState(0)
-  const [lenVen, setLenVen] = useState(0)
-  const [role, setRole] = useState("")
-  const [firstname,setFirstName] = useState("")
-  function convertStringFormatt(word) {
-    if (word == "" || word == null) {
-      return ""
-    } else {
-      let temp = word.slice(1, word.length - 1)
-      return temp
-    }
-  }
+const useFetchUser = (setEmail, setRole) => {
   useEffect(() => {
-    async function getUserEmail() {
-      const { data } = await supabase.auth.getUser()
+    const fetchUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setEmail(data?.user?.email || "");
 
-      let userJson = data.user
-
-      if (userJson != null) {
-        // let temp = convertStringFormatt(JSON.stringify(userJson.email))
-        setEmail(convertStringFormatt(JSON.stringify(data.user.email, null, 2)))
-
-        console.log("from get user email" + email)
-      }
-    }
-    async function getPublicUser() {
-      console.log("email: " + email)
-      const { data, error } = await supabase
+      const { data: users, error } = await supabase
         .from("User")
         .select()
-        .eq("email", email)
+        .eq("email", data?.user?.email);
 
       if (error) {
-        console.log("Error here" + error)
+        console.error(error);
+        return;
       }
 
-      if (data.length != 0) {
-        setRole(convertStringFormatt(JSON.stringify(data[0].role)))
-        console.log("role:" + role)
-      }
-    }
-    
-    async function getChatsAccordingToRole() {
-      console.log("pass mai")
-      if (role == "CUSTOMER") {
-        let slug = router.query
-        console.log("Customer id" + slug.chatid)
-        let { data: Chat, error } = await supabase
-          .from("Chat")
-          .select("*")
-          .eq("customer", slug.chatid)
-        if (error) {
-          console.log("Error in Chat" + JSON.stringify(error))
-        }
-        if (Chat == null) {
-          console.log("no chat yet")
-        } else {
-          setChats(Chat)
-          console.log(JSON.stringify(Chat))
-          setLenCus(Object.keys(Chat).length)
-          // console.log("len" + len)
-        }
-      }
-      if (role == "VENDOR") {
-        let slug = router.query
-        console.log("Customer id" + slug.chatid)
-        let { data: Chat, error } = await supabase
-          .from("Chat")
-          .select("*")
-          .eq("vendor", slug.chatid)
-        if (error) {
-          console.log("Error in Chat" + JSON.stringify(error))
-        }
-        if (Chat == null) {
-          console.log("no chat yet")
-        } else {
-          setChats(Chat)
-          console.log("Vendor chat "+JSON.stringify(Chat))
-          setLenVen(Object.keys(Chat).length)
-          // console.log("len" + len)
-        }
-      }
-    }
-    getChatsAccordingToRole()
-    getUserEmail()
-    getPublicUser()
-  }, [email,role, router,lenCus, lenVen])
+      setRole(users[0]?.role || "");
+    };
 
-  async function getFirstname(id){
-    let { data: Name, error } = await supabase
-    .from("User")
-    .select()
-    .eq("id", id)
-    if(Name !=null){
-      setFirstName(JSON.stringify(Name[0].firstname))
-    }
-  }
+    fetchUser();
+  }, []);
+};
 
-  const renderChat = () => {
-    let li = []
+const useFetchChats = (role, setChats, router) => {
+  useEffect(() => {
+    const fetchChats = async () => {
+      const { data: chats, error } = await supabase
+        .from("Chat")
+        .select("*")
+        .eq(role.toLowerCase(), router.query.chatid);
 
-    if (chats == null || chats.length == 0) {
-      return <p>No Chat yet</p>
-    } else {
-      let slug = router.query.chatid
-      let temp
-      if(lenCus){
-        temp = lenCus
+      if (error) {
+        console.error(error);
+        return;
       }
-      if(lenVen){
-        temp = lenVen
-      }
-      for (let i = 0; i < temp; i++) {
-        console.log("render pass" + JSON.stringify(chats[i].vendor))
-        if(role=="VENDOR"){
-          slug = slug + "/" + JSON.stringify(chats[i].customer)
-          let name = getFirstname(JSON.stringify(chats[i].customer))
-          li.push(
-            <ChatChannel
-              receiver={JSON.stringify(chats[i].customer)}
-              channel={slug}
-            ></ChatChannel>
-          )
-        }
-        if(role=="CUSTOMER"){
-          slug = slug + "/" + JSON.stringify(chats[i].vendor)
-          let name = getFirstname(JSON.stringify(chats[i].vendor))
 
-          li.push(
-            <ChatChannel
-              receiver={JSON.stringify(chats[i].vendor)}
-              channel={slug}
-            ></ChatChannel>
-          )
-        }
-        
-      }
-      return li
+      setChats(chats || []);
+    };
+
+    if (role) {
+      fetchChats();
     }
-  }
-  const chooseNavBar = () => {
-    if (role == "CUSTOMER") {
-      return <CustomerNavbar />
-    }
-    if (role == "VENDOR") {
-      return <VendorNavBar />
-    }
-  }
+  }, [role]);
+};
+
+
+
+export default function Chat() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [chats, setChats] = useState([]);
+
+  useFetchUser(setEmail, setRole);
+  useFetchChats(role, setChats, router);
+
+  const renderChat = chats.map((chat) => {
+    const receiverId = role === "VENDOR" ? chat.customer : chat.vendor;
+    const channel = `${router.query.chatid}/${receiverId}`;
+
+    return (
+      <ChatChannel
+        key={channel}
+        receiverId={receiverId} // You'll have to fetch the name separately
+        channel={channel}
+      />
+    );
+  });
+
+  const Navbar = role === "CUSTOMER" ? CustomerNavbar : VendorNavBar;
+
   return (
     <div>
-      {chooseNavBar()}
-      <div className="divide-y-2">
-        {/* <p>Chat {router.query.chatid}</p> */}
-        {renderChat()}
-      </div>
+      <Navbar />
+      <div className="divide-y-2">{renderChat}</div>
     </div>
-  )
+  );
 }
