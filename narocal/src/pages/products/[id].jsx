1aafe489bdeeffb6611a5b5eb9562742bfe71862
Natthapong Lueangphumphitthaya <thenat2015@gmail.com>
2023-05-27 21:43:58 +0700

    connect chat button

--- narocal/src/pages/products/[id].jsx
+++ narocal/src/pages/products/[id].jsx
@@ -1,96 +1,221 @@
 import { useRouter } from "next/router";
-import Image from "next/image"
-import InitialNavbar from '@/components/InitialNavbar'
-import BlackShirt from "../assets/black-shirt.jpg"
-import DropDown from '@/components/DropDown'
-import ProductCard from '@/components/ProductCard'
-import {getProductByID} from "../../../dummy-data"
+import Image from "next/image";
+import InitialNavbar from "@/components/InitialNavbar";
+import BlackShirt from "../assets/black-shirt.jpg";
+import DropDown from "@/components/DropDown";
+import ProductCard from "@/components/ProductCard";
+import { getProductByID } from "../../../dummy-data";
 import { useEffect, useState } from "react";
-import { supabase } from "lib/supabaseClient"
+import { supabase } from "lib/supabaseClient";
 
-export default function ProductPage(){
-    const router = useRouter();
+export default function ProductPage() {
+  const router = useRouter();
 
-    const [product, setProduct] = useState([""]);
+  const [product, setProduct] = useState([""]);
+  const [userId, setUserId] = useState(0)
+  const [email, setEmail] = useState("")
 
-    useEffect(() => {
-        async function renderInformation(){
-            const { data, error } = await supabase
-                .from("Product")
-                .select()
-                .eq("id", router.query.id)
-            
-            if(data == null){
-                console.log("pass");
-            }else{
-                setProduct(data);
-                
-            }
+  function convertStringFormatt(word) {
+    if (word == "" || word == null) {
+      return ""
+    } else {
+      let temp = word.slice(1, word.length - 1)
+      return temp
+    }
+  }
 
-            if (error) {
-                console.log(JSON.stringify(error))
-            }
-            
+  useEffect(() => {
+    async function renderInformation() {
+      const { data, error } = await supabase
+        .from("Product")
+        .select()
+        .eq("id", router.query.id);
+
+      if (data == null) {
+        console.log("pass");
+      } else {
+        setProduct(data);
+      }
+
+      if (error) {
+        console.log(JSON.stringify(error));
+      }
+    }
+    async function getUserEmail() {
+      const { data } = await supabase.auth.getUser();
+
+      let userJson = data.user;
+
+      // alert(JSON.stringify(data.user.email))
+      // alert(JSON.stringify(userJson.email))
+
+      if (userJson != null) {
+        // let temp = convertStringFormatt(JSON.stringify(userJson.email))
+        setEmail(convertStringFormatt(JSON.stringify(userJson.email, null, 2)));
+        console.log("from get user email" + email);
+      }
+    }
+    async function getPublicUser() {
+      console.log("email: " + email);
+      const { data, error } = await supabase
+        .from("User")
+        .select()
+        .eq("email", email);
+      // console.log("cus id" + JSON.stringify(data[0].id, null, 2))
+      console.log("cus id" + email);
+
+      if (error) {
+        console.log("Error" + error);
+      }
+
+      if (data.length != 0) {
+        setUserId(JSON.stringify(data[0].id, null, 2));
+        if (data[0].id == null) {
+          setUserId("");
         }
-        
-        renderInformation()
+      }
+    }
+
+    renderInformation();
+    getUserEmail();
+    getPublicUser();
+  }, [userId, email]);
+
+  const insertChat = async () => {
+    let slug = router.query
+    let vendorId = 0
+
+    product.map((p) => {
+        vendorId = p.vendorId
     })
 
-    return(
-        <>
-            { product.map((p) => {
-                return (
-                    <div>                    
-                        <InitialNavbar></InitialNavbar>
-                        <div className="flex flex-col justify-center gap-10">
-                            <div className="flex flex-row justify-center pt-40">
-                                <Image className="mx-14" src={p.image} alt="PRODUCT PICTURE" width={450} height={640}/>
-                                <div className="flex flex-col w-2/5">
-                                    <p className="text-xl font-bold">{p.brand}</p>
-                                    <p className="text-lg">{p.model}</p>
-                                    <p className="text-2xl py-10">{p.price}</p>
-                                    <select className="select w-full max-w-xs">
-                                        <option disabled selected>Select size</option>
-                                        <option>XS</option>
-                                        <option>S</option>
-                                        <option>M</option>
-                                        <option>L</option>
-                                        <option>XL</option>
-                                    </select>
+    // const { data: vendorData, error: errorVendor } = await supabase
+    //     .from("User")
+    //     .select()
+    //     .eq("id", vendorId);
+    
+    console.log("check " + vendorId)
 
-                                    <button className="btn btn-primary my-10 w-40">CHAT</button>
+    const { data, error } = await supabase
+      .from("Chat")
+      .upsert([{ vendor: vendorId, customer: userId }])
+    if (error) {
+      console.log("error in insert chat")
+    }
+  }
 
-                                    <div className="flex flex-col gap-2">
-                                        <div className="underline underline-offset-4 decoration-2">
-                                            Description
-                                        </div>
-                                        <div>
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure magnam libero quibusdam ipsa adipisci ullam, quia facere dignissimos ex temporibus deleniti molestias quidem ipsam, maxime voluptas atque maiores numquam repellat?
-                                        </div>
-                                    </div>
-                                    
+  const clickChat = async () => {
+    let slug = router.query;
+    let vendorId = 0
 
-                                </div>
+    product.map((p) => {
+        vendorId = p.vendorId
+    })
 
-                            </div>
+    const { data, error } = await supabase
+        .from("User")
+        .select()
+        .eq("id", vendorId);
 
-                            <div className="flex flex-col gap-2 justify-center">
-                                <div className="divider text-center underline underline-offset-4 decoration-2">
-                                    Recommend Items
-                                </div>
-                                <div className="flex flex-row gap-5 w-full justify-center">
-                                    <ProductCard title='Prada' body='Double Match cotton shirt'></ProductCard>
-                                    <ProductCard title='Louis Vuitton' body='Monogram Bandana shirt'></ProductCard>
-                                    <ProductCard title='Balenciaga' body='logo-print zip-up jacket'></ProductCard>
-                                    <ProductCard title='Maison Margiela' body='double-breasted tailored coat'></ProductCard>
-                                </div>
-                            </div>
+    console.log("check " + userId + vendorId);
 
-                        </div>
+    const { data: chat, error: chatError } = await supabase
+      .from("Chat")
+      .select()
+      .eq("vendor", vendorId)
+      .eq("customer", userId);
 
+    if (chat == null || chat.length == 0) {
+      insertChat();
+      console.log("null" + chat);
+      router.push("/chat/" + userId);
+    } else if (chatError) {
+      console.log(chatError);
+    } else {
+      console.log(JSON.stringify(chat));
+      router.push("/chat/" + userId);
+    }
+  };
+
+  return (
+    <>
+      {product.map((p) => {
+        return (
+          <div>
+            <InitialNavbar></InitialNavbar>
+            <div className="flex flex-col justify-center gap-10">
+              <div className="flex flex-row justify-center pt-40">
+                <Image
+                  className="mx-14"
+                  src={p.image}
+                  alt="PRODUCT PICTURE"
+                  width={450}
+                  height={640}
+                />
+                <div className="flex flex-col w-2/5">
+                  <p className="text-xl font-bold">{p.brand}</p>
+                  <p className="text-lg">{p.model}</p>
+                  <p className="text-2xl py-10">{p.price}</p>
+                  <select className="select w-full max-w-xs">
+                    <option disabled selected>
+                      Select size
+                    </option>
+                    <option>XS</option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                  </select>
+
+                  {/* <button className="btn btn-primary my-10 w-40" href="">CHAT</button> */}
+                  <input
+                    type="button"
+                    value="Chat"
+                    className="btn btn-outline btn-secondary"
+                    onClick={clickChat}
+                  />
+
+                  <div className="flex flex-col gap-2">
+                    <div className="underline underline-offset-4 decoration-2">
+                      Description
                     </div>
-                )
-            })}
-        </>
-    );
+                    <div>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Iure magnam libero quibusdam ipsa adipisci ullam, quia
+                      facere dignissimos ex temporibus deleniti molestias quidem
+                      ipsam, maxime voluptas atque maiores numquam repellat?
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2 justify-center">
+                <div className="divider text-center underline underline-offset-4 decoration-2">
+                  Recommend Items
+                </div>
+                <div className="flex flex-row gap-5 w-full justify-center">
+                  <ProductCard
+                    title="Prada"
+                    body="Double Match cotton shirt"
+                  ></ProductCard>
+                  <ProductCard
+                    title="Louis Vuitton"
+                    body="Monogram Bandana shirt"
+                  ></ProductCard>
+                  <ProductCard
+                    title="Balenciaga"
+                    body="logo-print zip-up jacket"
+                  ></ProductCard>
+                  <ProductCard
+                    title="Maison Margiela"
+                    body="double-breasted tailored coat"
+                  ></ProductCard>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
