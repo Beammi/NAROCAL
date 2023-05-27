6b3df8e64a2af911540fb47c7db0d769692da747
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-05-27 08:15:50 +0700

    Add Order Page

---
+++ narocal/src/pages/order/orderCustomerSide.jsx
@@ -0,0 +1,12 @@
+import CustomerNavbar from "@/components/CustomerNavbar"
+
+export default function OrderCustomer() {
+  return (
+    <div>
+      <CustomerNavbar></CustomerNavbar>
+      <div className="divide-y-2">
+        <h2>Order Customer Side</h2>
+      </div>
+    </div>
+  )
+}
---
+++ narocal/src/pages/order/orderVendorSide.jsx
@@ -0,0 +1,14 @@
+import VendorNavBar from "@/components/vendors/VendorNavBar"
+
+export default function OrderVendor() {
+  return (
+    <div>
+      <VendorNavBar></VendorNavBar>
+      <div className="divide-y-2">
+        <div className="p-6">
+          <h2>Order Vendor Side</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
